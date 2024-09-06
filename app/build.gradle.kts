d08e61a214767ac07fecca5a428f85f1fcf9d94b
James Sentinella <james@sentinella.me>
2024-09-05 18:23:35 -0700

    Updating version to version 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.chronokeep.registration"
         minSdk = 22
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
