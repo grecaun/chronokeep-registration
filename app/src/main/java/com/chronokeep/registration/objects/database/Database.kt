f104ddc8b5008b6640cd124a6eb7284e2eafbf5f
James Sentinella <james@sentinella.me>
2025-05-15 15:53:42 -0700

    Fixed migration issues.

--- app/src/main/java/com/chronokeep/registration/objects/database/Database.kt
+++ app/src/main/java/com/chronokeep/registration/objects/database/Database.kt
@@ -24,10 +24,12 @@ val migration_4_5 = object : Migration(4, 5) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE participant RENAME TO participant_old;")
         database.execSQL("UPDATE participant_old SET registration_id = first_name || last_name || distance || birthdate || chronokeep_info WHERE registration_id='';")
-        database.execSQL("CREATE TABLE IF NOT EXISTS `participant` (`row_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `registration_id` TEXT NOT NULL, `bib` TEXT NOT NULL, `first_name` TEXT NOT NULL, `last_name` TEXT NOT NULL, `birthdate` TEXT NOT NULL, `gender` TEXT NOT NULL, `distance` TEXT NOT NULL, `mobile` TEXT NOT NULL, `sms_enabled` INTEGER NOT NULL, `apparel` TEXT NOT NULL, `updated_at` INTEGER NOT NULL DEFAULT 0, `chronokeep_info` TEXT NOT NULL DEFAULT '', `uploaded` INTEGER NOT NULL DEFAULT false)")
-        database.execSQL("INSERT INTO participant SELECT * FROM participant_old;")
+        database.execSQL("CREATE TABLE IF NOT EXISTS participant (row_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, registration_id TEXT NOT NULL, bib TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, birthdate TEXT NOT NULL, gender TEXT NOT NULL, distance TEXT NOT NULL, mobile TEXT NOT NULL, sms_enabled INTEGER NOT NULL, apparel TEXT NOT NULL, updated_at INTEGER NOT NULL DEFAULT 0, chronokeep_info TEXT NOT NULL DEFAULT '', uploaded INTEGER NOT NULL DEFAULT false)")
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_participant_first_name_last_name_distance_birthdate_chronokeep_info ON participant (first_name, last_name, distance, birthdate, chronokeep_info)")
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_participant_registration_id ON participant (registration_id)")
+        database.execSQL("INSERT OR IGNORE INTO participant (row_id, registration_id, bib, first_name, last_name, birthdate, gender, distance, mobile, sms_enabled, apparel, updated_at, chronokeep_info, uploaded) SELECT row_id, registration_id, bib, first_name, last_name, birthdate, gender, distance, mobile, sms_enabled, apparel, updated_at, chronokeep_info, uploaded FROM participant_old;")
         database.execSQL("DROP TABLE participant_old;")
-        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS `index_participant_first_name_last_name_distance_birthdate_chronokeep_info` ON `participant` (`first_name`, `last_name`, `distance`, `birthdate`, `chronokeep_info`)")
-        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS `index_participant_registration_id` ON `participant` (`registration_id`)")
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_participant_first_name_last_name_distance_birthdate_chronokeep_info ON participant (first_name, last_name, distance, birthdate, chronokeep_info)")
+        database.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_participant_registration_id ON participant (registration_id)")
     }
 }
