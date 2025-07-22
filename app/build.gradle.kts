1d0d5547c462e34929cd34af58adaf057ce2cf7a
James Sentinella <james@sentinella.me>
2025-07-21 20:47:01 -0700

    Update version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.chronokeep.registration"
         minSdk = 22
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.1"
+        versionCode = 12
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
